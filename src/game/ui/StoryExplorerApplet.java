bddef91dcc40c955644352ca9e6a84c98d2ba706
Gail Carmichael <gail.banaszkiewicz@gmail.com>
2015-03-03 11:20:22 -0500

    Initial commit.

---
+++ src/game/ui/StoryExplorerApplet.java
@@ -0,0 +1,34 @@
+package game.ui;
+
+import processing.core.PApplet;
+
+public class StoryExplorerApplet extends PApplet
+{
+	private static final long serialVersionUID = 8907928995124251242L;
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	// Model
+	
+	// UI Stuff
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+	public void setup()
+	{
+	}
+	
+	public void draw()
+	{
+		
+	}
+	
+	///////////////////////////////////////////////////////////////////////////
+	
+
+	public static void main(String[] args) 
+	{
+		PApplet.main(StoryExplorerApplet.class.getCanonicalName());
+	}
+
+}
